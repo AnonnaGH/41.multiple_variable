c2f9686d255a79bd498587e6e36555aa7cd3af2e
“Anonna_Ferdaus” <anonnatahsin132@gmail.com>
2021-04-05 21:25:00 +0600

    first commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+var (
+   name string
+   age int
+   email string 
+)
+
+func main() {
+
+    name = "AnonnoTa"
+    age= 2
+    email="anonnotabd@gmail.com"
+
+
+fmt.Println(name)
+fmt.Println(age)
+fmt.Println(email)
+
+
+
+}
